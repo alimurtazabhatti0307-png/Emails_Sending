99dc9fe0c7c18a6cfa2265c9ac93d56211426dff
alimurtazabhatti0307-png <alimurtazabhatti0307@gmail.com>
2026-01-07 11:44:07 +0500

    Update index.js
    function code

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-export default async ({ req, res, log }) => {
-  log("Function executed successfully!");
+import nodemailer from "nodemailer";
 
-  return res.json({
-    success: true,
-    message: "Function ran successfully",
-  });
+export default async ({ req, res, log }) => {
+  try {
+    log("Email function started");
+
+    const { to, subject, message } = JSON.parse(req.body || "{}");
+
+    if (!to || !subject || !message) {
+      return res.json({
+        success: false,
+        error: "Missing email data",
+      });
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: `"Appwrite Mailer" <${process.env.SMTP_USER}>`,
+      to,
+      subject,
+      text: message,
+    });
+
+    log("Email sent successfully");
+
+    return res.json({
+      success: true,
+      message: "Email sent successfully",
+    });
+
+  } catch (error) {
+    log(error.message);
+
+    return res.json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
